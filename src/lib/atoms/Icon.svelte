51be7405d1fae5ebdc2dcb67837128bac59486d1
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-11-16 18:09:08 +0100

    ⚛️ Added Icon Atom

---
+++ src/lib/atoms/Icon.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let icon
+</script>
+
+<div class="icon">
+    <img src="/images/icons/{icon}" alt="">
+</div>
+
+<style>
+    .icon {
+        width: var(--icon-width, 0.8rem);
+        height: auto
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Button from '$lib/atoms/Button.svelte';
+	import Icon from '$lib/atoms/Icon.svelte';
 </script>
 
+<!-- Button -->
 <Button btnType="text" btnText="Inloggen"></Button>
 <!-- <Button btnType="text" btnText="Inloggen" --btn-color="green"></Button> -->
+
+<!-- Icon -->
+<Icon icon="arrow-right.svg" --icon-width="1.6rem"></Icon>
