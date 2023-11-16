95d5c0065ca3db9dc5c033d0d4b5c3d5d76ae3c9
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-11-16 16:44:26 +0100

    ⚛️ Added Buttom Atom

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Button from '$lib/atoms/Button.svelte';
+</script>
+
+<Button btnType="text" btnText="Inloggen"></Button>
+<!-- <Button btnType="text" btnText="Inloggen" --btn-color="green"></Button> -->
